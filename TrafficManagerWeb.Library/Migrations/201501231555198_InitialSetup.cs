adb17fb3a25d7925c03a7107ea19d314e941856b
mszcool <marioszp@microsoft.com>
2015-01-23 17:33:49 +0100

    Updating Code-First Migrations and Database Script. Now should be ready for deployment scripting.

--- TrafficManagerWeb.Library/Migrations/201501231555198_InitialSetup.cs
+++ TrafficManagerWeb.Library/Migrations/201501231555198_InitialSetup.cs
@@ -7,10 +7,22 @@ namespace TrafficManagerWeb.Library.Migrations
     {
         public override void Up()
         {
+            base.CreateTable("dbo.LogEntities", c => new
+                    {
+                        LogId = c.Int(nullable: false, identity: true),
+                        LogDate = c.DateTime(nullable: false),
+                        LogSiteSource = c.String(nullable: false),
+                        LogSiteRegion = c.String(nullable: false),
+                        LogMessage = c.String(nullable:false)
+                    })
+                    .PrimaryKey(t => t.LogId)
+                    .Index(t => t.LogDate);
         }
         
         public override void Down()
         {
+            base.DropIndex("dbo.LogEntities", new[] { "LogDate" });
+            base.DropTable("dbo.LogEntities");
         }
     }
 }
