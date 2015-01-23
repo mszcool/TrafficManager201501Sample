c8464f2d7e315b3103ad57912945dadf2a5e03d1
mszcool <marioszp@microsoft.com>
2015-01-23 17:03:00 +0100

    Adding SqlLogDataDal since it was excluded in previous commit

---
+++ TrafficManagerWeb.Library/DataAccess/SqlLogDataDal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using TrafficManagerWeb.Library.DataAccess.Interfaces;
+using TrafficManagerWeb.Library.Entities;
+
+namespace TrafficManagerWeb.Library.DataAccess
+{
+    public class SqlLogDataDal : ILogDataAccess
+    {
+        private SqlLogDataDbContext _dbContext;
+
+        public SqlLogDataDal()
+        {
+            _dbContext = new SqlLogDataDbContext();
+        }
+
+        public Task<List<Entities.LogEntity>> GetLogEntitiesAsync()
+        {
+            return Task.Run<List<Entities.LogEntity>>(() =>
+            {
+                var logEntities = _dbContext.LogEntries.ToList();
+                return logEntities;
+            });
+        }
+
+        public Task AddLogEntityAsync(Entities.LogEntity entity)
+        {
+            return Task.Run(() =>
+            {
+                _dbContext.LogEntries.Add(entity);
+                _dbContext.SaveChanges();
+            });
+        }
+    }
+
+    internal class SqlLogDataDbContext : DbContext
+    {
+        public DbSet<LogEntity> LogEntries { get; set; }
+
+        public SqlLogDataDbContext()
+            : base("name=TrafficManagerSample.LogDataStorageConnection")
+        {
+        }
+    }
+}
